6b8a346a931c1546705ac739b04da408a1e2633a
JayV0128 <110962603+JayV0128@users.noreply.github.com>
2022-08-11 12:36:39 +0800

    Update AddItem.php

--- AddItem.php
+++ AddItem.php
@@ -3,7 +3,14 @@
     if (!empty($_POST)) {
         var_dump($_POST);
         extract($_POST);
-        $sql = "INSERT INTO item(itemID, itemName, itemDescription, stockQuantity, price) VALUES ($itemID, '$itemName', '$itemDesc', $stockQuantity, $price)";
+
+        $query = "SELECT MAX(itemID) + 1 AS 'max' FROM Item";
+        $result = mysqli_query($conn, $query);
+        while ($rc = mysqli_fetch_assoc($result)) {
+            extract($rc);
+        }
+
+        $sql = "INSERT INTO item(itemID, itemName, itemDescription, stockQuantity, price) VALUES ($max, '$itemName', '$itemDesc', $stockQuantity, $price)";
         $rs = mysqli_query($conn, $sql);
     }
     mysqli_close($conn);
